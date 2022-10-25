38cc00bcb5d5e3749cd52bd2be834cd1ce9f8b47
ivanfermena <ivanfermena@gmail.com>
2022-10-25 18:53:43 +0200

    Select query

--- lambda/aws-lambda-url.py
+++ lambda/aws-lambda-url.py
@@ -7,28 +7,40 @@ username = os.environ.get('RDS_DB_USERNAME')
 password = os.environ.get('RDS_DB_PASSWORD')
 db_name = os.environ.get('RDS_DB_NAME')
 
-try:
-    connection = psycopg2.connect(user=username,
-                                  password=password,
-                                  host=rds_host,
-                                  port="5432",
-                                  database=db_name)
+def lambda_handler(event, context):
+    try:
+        connection = psycopg2.connect(user=username,
+                                      password=password,
+                                      host=rds_host,
+                                      port="5432",
+                                      database=db_name)
 
-    cursor = connection.cursor()
-    # SQL query to create a new table
-    create_table_query = '''CREATE TABLE mobile
-          (ID INT PRIMARY KEY     NOT NULL,
-          MODEL           TEXT    NOT NULL,
-          PRICE         REAL); '''
-    # Execute a command: this creates a new table
-    cursor.execute(create_table_query)
-    connection.commit()
-    print("Table created successfully in PostgreSQL ")
+        cursor = connection.cursor()
+        # SQL query to create a new table
+        create_table_query = '''CREATE TABLE mobile
+              (ID INT PRIMARY KEY     NOT NULL,
+              MODEL           TEXT    NOT NULL,
+              PRICE         REAL); '''
+        # Execute a command: this creates a new table
+        cursor.execute(create_table_query)
+        connection.commit()
+        print("Table created successfully in PostgreSQL ")
 
-except (Exception, Error) as error:
-    print("Error while connecting to PostgreSQL", error)
-finally:
-    if connection:
-        cursor.close()
-        connection.close()
-        print("PostgreSQL connection is closed")
+        cursor = connection.cursor()
+        # Executing a SQL query to insert data into  table
+        insert_query = """ INSERT INTO mobile (ID, MODEL, PRICE) VALUES (1, 'Iphone12', 1100)"""
+        cursor.execute(insert_query)
+        connection.commit()
+        print("1 Record inserted successfully")
+        # Fetch result
+        cursor.execute("SELECT * from mobile")
+        record = cursor.fetchall()
+        print("Result ", record)
+
+    except (Exception, Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
